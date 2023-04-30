4bca472d22a76870e4572a440b36fe0c1dbced6a
Fu-Te <37261985+T3pp31@users.noreply.github.com>
2023-04-30 23:32:39 +0900

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("portfolio/", include("portfolio.urls")),
+    path("", include("portfolio.urls")),
 ]
 
 if settings.DEBUG:

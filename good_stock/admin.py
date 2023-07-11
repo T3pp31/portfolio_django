4df1c969f3273409e468086abf31b5b63b8a2b67
Fu-Te <37261985+T3pp31@users.noreply.github.com>
2023-07-12 00:53:39 +0900

    add

--- good_stock/admin.py
+++ good_stock/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from import_export.admin import ImportExportMixin, ImportMixin
+from import_export.admin import ImportMixin
+from import_export.fields import Field
 from import_export.formats import base_formats
 from import_export.resources import ModelResource
 
@@ -8,26 +9,103 @@ from .models import *
 
 # Register your models here.
 class FinancialInfoResource(ModelResource):
+    stock_id = Field(attribute="stock_id", column_name="symbol")
+    date = Field(attribute="date", column_name="asOfDate")
+    total_revenue = Field(
+        attribute="total_revenue", column_name="TotalRevenue"
+    )
+    stockholdersequity = Field(
+        attribute="stockholdersequity", column_name="StockholdersEquity"
+    )
+    totalassets = Field(attribute="totalassets", column_name="TotalAssets")
+    capitaladequancyratio = Field(
+        attribute="capitaladequancyratio", column_name="capitalAdequacyRatio"
+    )
+    roe = Field(attribute="roe", column_name="ROE")
+
     class Meta:
         model = FinancialInfo
 
+        import_order = (
+            "stock_id",
+            "date",
+            "total_revenue",
+            "stockholdersequity",
+            "totalassets",
+            "capitaladequancyratio",
+            "roe",
+        )
+        import_id_fields = ["stock_id"]
+
+
+class FinancialInfoAdmin(ImportMixin, admin.ModelAdmin):
+    list_display = (
+        "stock_id",
+        "date",
+        "total_revenue",
+        "stockholdersequity",
+        "totalassets",
+        "capitaladequancyratio",
+        "roe",
+    )
+    resource_class = FinancialInfoResource
+    formats = [base_formats.CSV]
+
 
 class MetricsResource(ModelResource):
+    stock_id = Field(attribute="stock_id", column_name="ticker")
+    stock_name = Field(attribute="stock_name", column_name="ticker_name")
+    market = Field(attribute="market", column_name="market_product_category")
+    stock_category33 = Field(
+        attribute="stock_category33", column_name="type_33"
+    )
+    stock_category17 = Field(
+        attribute="stock_category17", column_name="type_17"
+    )
+    dividendRate = Field(attribute="dividendRate", column_name="dividendRate")
+    dividendYield = Field(
+        attribute="dividendYield", column_name="dividendYield"
+    )
+    fiveYearAvgDividendYield = Field(
+        attribute="fiveYearAvgDividendYield",
+        column_name="fiveYearAvgDividendYield",
+    )
+    payoutRatio = Field(attribute="payoutRatio", column_name="payoutRatio")
+    marketcap = Field(attribute="marketcap", column_name="MarketCap")
+    totalRevenue = Field(attribute="totalRevenue", column_name="totalRevenue")
+    roe = Field(attribute="roe", column_name="ROE")
+
     class Meta:
         model = Metrics
 
+        import_order = (
+            "stock_id",
+            "stock_name",
+            "market",
+            "stock_category33",
+            "stock_category17",
+            "dividendRate",
+            "dividendYield",
+            "fiveYearAvgDividendYield",
+            "payoutRatio",
+            "marketcap",
+            "totalRevenue",
+            "roe",
+        )
+        import_id_fields = ["stock_id"]
 
-@admin.register(FinancialInfo)
-class FinancialInfoResourceAdmin(ImportExportMixin, admin.ModelAdmin):
-    resource_class = FinancialInfoResource
 
-
-@admin.register(Metrics)
-class MetricsResourceAdmin(ImportExportMixin, admin.ModelAdmin):
+class MetricsAdmin(ImportMixin, admin.ModelAdmin):
+    list_display = (
+        "stock_id",
+        "stock_name",
+        "market",
+        "stock_category33",
+        "stock_category17",
+    )
     resource_class = MetricsResource
+    formats = [base_formats.CSV]
 
 
-admin.site.register(FinancialInfo)
-admin.site.register(Metrics)
-admin.site.register(FinancialInfoResourceAdmin)
-admin.site.register(MetricsResourceAdmin)
+admin.site.register(FinancialInfo, FinancialInfoAdmin)
+admin.site.register(Metrics, MetricsAdmin)
--- good_stock/models.py
+++ good_stock/models.py
@@ -3,25 +3,20 @@ from django.db import models
 
 # Create your models here.
 class Metrics(models.Model):
-    stock_id = models.CharField(max_length=100)
-    stock_name = models.CharField(max_length=100)
-    market = models.CharField(max_length=100)
-    stock_category33 = models.CharField(max_length=100)
-    stock_category17 = models.CharField(max_length=100)
-
-    dividendRate = models.IntegerField()
-
-    dividendYield = models.IntegerField()
-
-    fiveYearAvgDividendYield = models.IntegerField()
-
-    payoutRatio = models.IntegerField()
-
-    marketcap = models.IntegerField()
-
-    totalRevenue = models.IntegerField()
-
-    roe = models.IntegerField()
+    stock_id = models.CharField(max_length=100, null=True, blank=True)
+    stock_name = models.CharField(max_length=100, null=True, blank=True)
+    market = models.CharField(max_length=100, null=True, blank=True)
+    stock_category33 = models.CharField(max_length=100, null=True, blank=True)
+    stock_category17 = models.CharField(max_length=100, null=True, blank=True)
+    dividendRate = models.CharField(max_length=100, null=True, blank=True)
+    dividendYield = models.CharField(max_length=100, null=True, blank=True)
+    fiveYearAvgDividendYield = models.CharField(
+        max_length=100, null=True, blank=True
+    )
+    payoutRatio = models.CharField(max_length=100, null=True, blank=True)
+    marketcap = models.CharField(max_length=100, null=True, blank=True)
+    totalRevenue = models.CharField(max_length=100, null=True, blank=True)
+    roe = models.CharField(max_length=100, null=True, blank=True)
 
     class Meta:
         verbose_name = "Metrics"
@@ -31,13 +26,17 @@ class Metrics(models.Model):
 
 
 class FinancialInfo(models.Model):
-    stock_id = models.CharField(max_length=100)
-    date = models.DateTimeField()
-    total_revenue = models.IntegerField()
-    stockholdersequity = models.IntegerField()
-    totalassets = models.IntegerField()
-    capitaladequancyratio = models.IntegerField()
-    roe = models.IntegerField()
+    stock_id = models.CharField(max_length=100, null=True, blank=True)
+    date = models.DateTimeField(null=True, blank=True)
+    total_revenue = models.CharField(max_length=100, null=True, blank=True)
+    stockholdersequity = models.CharField(
+        max_length=100, null=True, blank=True
+    )
+    totalassets = models.CharField(max_length=100, null=True, blank=True)
+    capitaladequancyratio = models.CharField(
+        max_length=100, null=True, blank=True
+    )
+    roe = models.CharField(max_length=100, null=True, blank=True)
 
     class Meta:
         verbose_name = "FinancialInfo"
